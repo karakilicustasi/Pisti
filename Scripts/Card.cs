209972d8b797e25ee4cae25e7bd9c49081881e21
Yıldırım Çağlar DAŞDEMİR <yildirimcaglardasdemir@gmail.com>
2021-04-24 06:12:58 +0300

    Refactoring on card and deck class

--- Scripts/Card.cs
+++ Scripts/Card.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 public class Card
 {
-    private int type {get; set;}
-    private int value {get; set;}
+    private int type; //0- hearts 1-clubs 2- diamonds 3-spades
+    private int value;//1-10 -> 11 - J 12 - Q 13 - K
 
-    public Card(){
-
-    }
-    public Card(int type,int value){
-        this.type = type;
-        this.value = value;
-    }
+    
 }
