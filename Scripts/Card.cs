69a07fbdd1857aa6f1ac349518828713aa1a0ade
Yıldırım Çağlar DAŞDEMİR <yildirimcaglardasdemir@gmail.com>
2021-04-24 05:41:56 +0300

    Initial Commit for Pisti

---
+++ Scripts/Card.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card
+{
+    private int type {get; set;};
+    private int value {get; set;};
+
+    public Card(){
+
+    }
+    public Card(int type,int value){
+        this.type = type;
+        this.value = value;
+    }
+}
